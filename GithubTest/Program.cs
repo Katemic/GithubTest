81ec34fe07058b2ce27f73a48108cc7c1d37e9d3
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2024-09-09 10:30:30 +0200

    feature

--- GithubTest/Program.cs
+++ GithubTest/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, feature udvikling!");
+
+int number = 0;
+while (number < 10)
+{
+
+    Console.WriteLine("Hello");
+    Console.WriteLine("hehehe");
+    number++;
+}
